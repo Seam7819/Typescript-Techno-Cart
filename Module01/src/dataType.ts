a0479bd77860903be7809174d480f29801db1a40
Snu Seam <siamahme766@gmail.com>
2025-05-22 12:58:24 +0600

    basic data type in ts

---
+++ Module01/src/dataType.ts
@@ -0,0 +1,22 @@
+// Basic Data Types 
+
+
+// primitives data types 
+let number : number = 35; 
+let myName : string = "Mr Siam";
+let isLearning : boolean = true;
+let nul : null = null;
+let undefine : undefined  = undefined;
+
+
+
+// non primitives data types 
+
+
+// Array : String type array
+let rollNumber : number[] = [12,23,45,22,23,55,23,45];
+let names : string[] = ["sajna","rajna","kangana","ratna"];
+
+//  tuple type 
+let nameAge : [string,number] = [ "sokina", 24]; 
+let ageNamePresent : [number,string, boolean] = [23,"kabila", true];
