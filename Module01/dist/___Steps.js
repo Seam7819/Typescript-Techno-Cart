88b0375c65ef633034ed2a20b75f93ee40ae366c
Snu Seam <siamahme766@gmail.com>
2025-05-22 11:45:36 +0600

    commit steps of ts

---
+++ Module01/dist/___Steps.js
@@ -0,0 +1,11 @@
+// At first open folder in vs
+// create a file
+// typescript file does not run by node
+// if we run node file.ts we will get error
+//  we have to run typescript file by "TSC(tsc)"
+// if we write tsc module\file.ts our file convert into js
+// to config our files we have to initialized ts compiler
+//  tsc --init     --> for config file
+// in config file we have to keep ts file one place and keep js file to another place
+//  rootdir ts file and outdir will be js file
+// then we have to write tsc in terminal
