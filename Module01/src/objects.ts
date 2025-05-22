ce229c1dc2ef56bb2745ea8b3a96dc901d95bab9
Snu Seam <siamahme766@gmail.com>
2025-05-22 21:14:26 +0600

    commit objectt type data

---
+++ Module01/src/objects.ts
@@ -0,0 +1,13 @@
+// Reference type object 
+
+// This is called explicit data type when we tell the type 
+const user: {
+    name: "Sultan";  // this is literel data type. This is also a type is ts. 
+    readonly age: number; //this also cannot be changed because it is also a readonly prop
+    Institute: string;
+} = {
+    name : "Sultan",
+    age : 26,
+    Institute : "Green University"
+}
+
