cdfc589f5bbc68668848e3b014f44d52cefcada8
Snu Seam <siamahme766@gmail.com>
2025-05-29 23:47:28 +0600

    m

--- Module03/src/Polymorphism.ts
+++ Module03/src/Polymorphism.ts
@@ -2,22 +2,22 @@
     // polymorphism 
 
     class People {
-        getSleep(){
+        getSleep() {
             console.log('I am sleeping for 8 hours');
         }
     }
-    class Student extends People{
-        getSleep(){
+    class Student extends People {
+        getSleep() {
             console.log('I am sleeping for 7 hours');
         }
     }
-    class Developer extends People{
-        getSleep(){
+    class Developer extends People {
+        getSleep() {
             console.log('I am sleeping for 6 hours');
         }
     }
 
-    const getSleepingHours = (params: People) =>{
+    const getSleepingHours = (params: People) => {
         params.getSleep()
     }
 
@@ -30,37 +30,37 @@
     getSleepingHours(person3);
 
     class Shape {
-        getResult(){
+        getResult() {
             return 0;
         }
     }
     class Rectangle extends Shape {
-        radius : number;
+        radius: number;
 
-        constructor (radius : number){
+        constructor(radius: number) {
             super()
             this.radius = radius
         }
-        getResult(){
+        getResult() {
             return Math.PI * this.radius * this.radius;
         }
     }
     class Area extends Shape {
-        radius : number;
+        radius: number;
 
-        constructor (radius : number){
+        constructor(radius: number) {
             super()
             this.radius = radius
         }
-        getResult(){
-            return 2* Math.PI * this.radius;
+        getResult() {
+            return 2 * Math.PI * this.radius;
         }
     }
 
-    const getResultOfSHape = (param : Shape)=>{
+    const getResultOfSHape = (param: Shape) => {
         console.log(param.getResult());
     }
-    
+
 
     const res1 = new Shape();
     const res2 = new Rectangle(12);
